e796bc7ce9702d15f138feb693eb12a149e331cc
Diana Ivanova <diana.website.developer@gmail.com>
2024-02-19 22:07:41 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 
-// main page
-const aboutMeTitle = document.querySelector('.aboutme__title');
-const aboutMeText = document.querySelector('.aboutme__description');
-const aboutMePortrait = document.querySelector('.aboutme__image');
-
-//blog page
-const articleTexts = document.querySelectorAll('.blog__article-text');
-const articleTitles = document.querySelectorAll('.blog_title');
 
 
 const currentUrl = window.location.href;
@@ -16,20 +8,40 @@ console.log(parts);
 const address = parts[parts.length - 1];
 console.log(address);
 
-async function fetchAllStories() {
-  try {
-    const response = await fetch('https://api.storyblok.com/v2/cdn/stories?version=draft&token=LeefeUkJwy0vrTrfJ8rhUAtt&cv=1708350835');
-    const data = await response.json();
-    console.log(data);
-    const stories = data.stories;
-    console.log(stories);
-    populateHtml(stories);
-    return stories;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
+const menuList = document.querySelector('.header__link-list');
+
+
+const colorArray = ['#0066ff', '#e37218', '#e73d71'];
+
+function handleHover(event, isHovering) {
+  if (event.target.tagName === 'A') {
+    if (isHovering) {
+      const randomIndex = Math.floor(Math.random() * colorArray.length);
+      const randomColor = colorArray[randomIndex];
+      event.target.style.color = randomColor;
+    } else {
+      event.target.style.color = '#000000';
+    }
   }
 }
+
+
+
+
+// async function fetchAllStories() {
+//   try {
+//     const response = await fetch('https://api.storyblok.com/v2/cdn/stories?version=draft&token=LeefeUkJwy0vrTrfJ8rhUAtt&cv=1708350835');
+//     const data = await response.json();
+//     console.log(data);
+//     const stories = data.stories;
+//     console.log(stories);
+//     populateHtml(stories);
+//     return stories;
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     return [];
+//   }
+// }
 
 // async function populateHtml() {
 //   const story = await fetchAllStories();
@@ -41,12 +53,22 @@ async function fetchAllStories() {
 //   // aboutMePortrait.src = image.filename;
 // }
 
-function populateHtml(content) {
-  console.log(content);
+// function populateHtml(content) {
+//   console.log(content);
 
-  content.forEach((page) => {
-    console.log(page);
-  })
+//   content.forEach((page) => {
+//     console.log(page);
+//   })
+// }
+
+// fetchAllStories();
+
+function checkIfPageExists() {
+  const validRoutes = ['/index.html', '/my-portfolio.html', '/page-contacts.html', '/page-portfolio.html', '/page-resume.html', '/page-blog.html', '/page-article.html'];
+  const path = window.location.pathname;
+  if (!validRoutes.includes(path)) {
+    window.location.href = '/404.html';
+  }
 }
 
-fetchAllStories();
+window.addEventListener('DOMContentLoaded', checkIfPageExists);
