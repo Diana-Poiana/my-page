5f14e14a34355a9b7eb65def983eb723ca0d76cb
Diana Ivanova <diana.website.developer@gmail.com>
2024-02-22 11:39:31 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -68,12 +68,12 @@ try {
 
 // fetchAllStories();
 
-function checkIfPageExists() {
-  const validRoutes = ['/index', '/my-portfolio', '/page-contacts', '/page-portfolio', '/page-resume', '/page-blog', '/page-article'];
-  const path = window.location.pathname;
-  if (!validRoutes.includes(path)) {
-    window.location.href = '/404';
-  }
-}
+// function checkIfPageExists() {
+//   const validRoutes = ['/index', '/my-portfolio', '/page-contacts', '/page-portfolio', '/page-resume', '/page-blog', '/page-article'];
+//   const path = window.location.pathname;
+//   if (!validRoutes.includes(path)) {
+//     window.location.href = '/404';
+//   }
+// }
 
-window.addEventListener('DOMContentLoaded', checkIfPageExists);
+// window.addEventListener('DOMContentLoaded', checkIfPageExists);
